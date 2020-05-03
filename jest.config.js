6c01635f260143a27beeeba4867090a512d758cf
Matan Lurey <matan@lurey.org>
2020-05-03 15:33:44 -0700

    Tweak scripts.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '(/test/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
